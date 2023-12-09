a557e5b245f0c7bb7b9f95a79c0f7a56c279b790
Joshua Baggott <jwbb1508@gmail.com>
2023-12-09 23:06:59 +0000

    Completed Day 3, closes #92

--- 2023/AdventOfCode.2023/03/EngineSchematic.cs
+++ 2023/AdventOfCode.2023/03/EngineSchematic.cs
@@ -9,12 +9,31 @@ namespace AdventOfCode._2023._03
 
         public override string GetAnswer()
         {
-            return Schematic.FromData(File.ReadAllLines(DataFilename))
-                .GetPartNumbers()
-                .Sum(x => x.Number)
-                .ToString();
+            IEnumerable<PartLabel> partLabels = Schematic.FromData(File.ReadAllLines(DataFilename)).GetPartLabels();
+
+            switch (Part)
+            {
+                case Part.One:
+                    return partLabels
+                        .GroupBy(x => (x.PartNumberY, x.PartNumberX))
+                        .Sum(x => x.First().PartNumber)
+                        .ToString();
+
+                case Part.Two:
+                    {
+                        return partLabels
+                            .GroupBy(x => (x.SymbolY, x.SymbolX))
+                            .Where(x => x.First().Symbol == '*')
+                            .Where(x => x.Count() == 2)
+                            .Sum(x => x.Aggregate(1, (product, symbol) => product * symbol.PartNumber))
+                            .ToString();
+                    }
+
+                default:
+                    throw new InvalidOperationException($"Unknown Part: {Part}");
+            }
         }
-        
+
         private class Schematic
         {
             private readonly char[][] _schematic;
@@ -29,9 +48,9 @@ namespace AdventOfCode._2023._03
                 return new Schematic(lines.Select(x => x.ToCharArray()).ToArray());
             }
 
-            public IEnumerable<(int Number, int Y, int X)> GetPartNumbers()
+            public IEnumerable<PartLabel> GetPartLabels()
             {
-                IList<(int Number, int Y, int X)> partNumbers = new List<(int, int, int)>();
+                IList<PartLabel> partLabels = new List<PartLabel>();
 
                 var y = 0;
                 while (y < _schematic.Length)
@@ -40,17 +59,20 @@ namespace AdventOfCode._2023._03
                     while (x < _schematic[y].Length)
                     {
                         string partNumber = string.Empty;
-                        bool isAdjacentToPart = false;
+                        ISet<(int Y, int X, char Symbol)> symbols = new HashSet<(int, int, char)>();
                         while (x < _schematic[y].Length && char.IsDigit(_schematic[y][x]))
                         {
-                            isAdjacentToPart = isAdjacentToPart || IsAdjacentToPart(y, x);
+                            symbols.UnionWith(GetAdjacentSymbols(y, x));
                             partNumber += _schematic[y][x];
                             x += 1;
                         }
 
-                        if (!string.IsNullOrEmpty(partNumber) && isAdjacentToPart)
+                        if (!string.IsNullOrEmpty(partNumber))
                         {
-                            partNumbers.Add((int.Parse(partNumber), y, x - partNumber.Length));
+                            foreach (var symbol in symbols)
+                            {
+                                partLabels.Add(new PartLabel(int.Parse(partNumber), y, x - partNumber.Length, symbol.Symbol, symbol.Y, symbol.X));
+                            }
                         }
 
                         x += 1;
@@ -59,11 +81,18 @@ namespace AdventOfCode._2023._03
                     y += 1;
                 }
 
-                return partNumbers;
+                return partLabels;
             }
 
-            private bool IsAdjacentToPart(int y, int x)
+            private static bool IsSymbol(char value)
             {
+                return !char.IsDigit(value) && value != '.';
+            }
+
+            private ISet<(int Y, int X, char Symbol)> GetAdjacentSymbols(int y, int x)
+            {
+                ISet<(int Y, int X, char Symbol)> symbols = new HashSet<(int, int, char)>();
+
                 var yValues = new[] { y - 1, y, y + 1 };
                 var xValues = new[] { x - 1, x, x + 1 };
 
@@ -83,48 +112,37 @@ namespace AdventOfCode._2023._03
 
                         if (IsSymbol(_schematic[yValue][xValue]))
                         {
-                            return true;
+                            symbols.Add((yValue, xValue, _schematic[yValue][xValue]));
                         }
                     }
                 }
 
-                return false;
+                return symbols;
             }
+        }
 
-            private bool IsSymbol(char value)
+        private class PartLabel
+        {
+            public int PartNumber { get; }
+
+            public int PartNumberY { get; }
+
+            public int PartNumberX { get; }
+
+            public char Symbol { get; }
+
+            public int SymbolY { get; }
+
+            public int SymbolX { get; }
+
+            public PartLabel(int partNumber, int partNumberY, int partNumberX, char symbol, int symbolY, int symbolX)
             {
-                return !char.IsDigit(value) && value != '.';
-            }
-
-            private short CountAdjacentPartNumbers(int y, int x)
-            {
-                var yValues = new[] { y - 1, y, y + 1 };
-                var xValues = new[] { x - 1, x, x + 1 };
-
-                var count = 0;
-
-                foreach (var yValue in yValues)
-                {
-                    if (yValue < 0 || yValue >= _schematic.Length)
-                    {
-                        continue;
-                    }
-
-                    foreach (var xValue in xValues)
-                    {
-                        if (xValue < 0 || xValue >= _schematic[yValue].Length)
-                        {
-                            continue;
-                        }
-
-                        if (char.IsDigit(_schematic[yValue][xValue]))
-                        {
-                            
-                        }
-                    }
-                }
-
-                return count;
+                PartNumber = partNumber;
+                PartNumberY = partNumberY;
+                PartNumberX = partNumberX;
+                Symbol = symbol;
+                SymbolY = symbolY;
+                SymbolX = symbolX;
             }
         }
     }
