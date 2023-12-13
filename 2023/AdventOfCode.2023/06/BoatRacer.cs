327fff79e20b43313f1cca6e8796e177261c31d4
Joshua Baggott <jwbb1508@gmail.com>
2023-12-13 00:07:06 +0000

    Completed Day 6, closes #95

---
+++ 2023/AdventOfCode.2023/06/BoatRacer.cs
@@ -0,0 +1,59 @@
+﻿using AdventOfCode._2023.Library;
+
+namespace AdventOfCode._2023._06
+{
+    internal class BoatRacer : APuzzle
+    {
+        public BoatRacer(string dataFilename, Part part) : base(dataFilename, part)
+        { }
+
+        public override string GetAnswer()
+        {
+            string[] lines = File.ReadAllLines(DataFilename);
+
+            long[] times = lines[0]
+                .Split((char[]?)null, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
+                .Skip(1)
+                .Select(long.Parse)
+                .ToArray();
+
+            if (Part == Part.Two)
+            {
+                times = new[] { long.Parse(string.Join(string.Empty, times)) };
+            }
+
+            long[] records = lines[1]
+                .Split((char[]?)null, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
+                .Skip(1)
+                .Select(long.Parse)
+                .ToArray();
+
+            if (Part == Part.Two)
+            {
+                records = new[] { long.Parse(string.Join(string.Empty, records)) };
+            }
+
+            long product = 1;
+            for (long i = 0; i < times.Length; i++)
+            {
+                long time = times[i];
+                long record = records[i];
+
+                long victories = 0;
+
+                for (long hold = 1; hold < time; hold++)
+                {
+                    long distance = (time - hold) * hold;
+                    if (distance > record)
+                    {
+                        victories++;
+                    }
+                }
+
+                product *= victories;
+            }
+
+            return product.ToString();
+        }
+    }
+}
--- 2023/AdventOfCode.2023/Library/PuzzleFactory.cs
+++ 2023/AdventOfCode.2023/Library/PuzzleFactory.cs
@@ -3,6 +3,7 @@ using AdventOfCode._2023._02;
 using AdventOfCode._2023._03;
 using AdventOfCode._2023._04;
 using AdventOfCode._2023._05;
+using AdventOfCode._2023._06;
 
 namespace AdventOfCode._2023.Library
 {
@@ -17,6 +18,7 @@ namespace AdventOfCode._2023.Library
                 3 => new EngineSchematic(dataFilename, part),
                 4 => new ScratchcardResolver(dataFilename, part),
                 5 => new GardenMapper(dataFilename, part),
+                6 => new BoatRacer(dataFilename, part),
                 _ => throw new ArgumentOutOfRangeException(nameof(day), day, $"Day {day} not supported by PuzzleFactory"),
             };
         }
